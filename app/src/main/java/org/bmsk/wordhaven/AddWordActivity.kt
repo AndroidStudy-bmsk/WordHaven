15048b12850e0c99e2010bf31ec60e5092664933
bmsk <vv99911@gmail.com>
2023-03-17 21:40:10 +0900

    [Add] Added "AddWordActivity"

--- app/src/main/java/org/bmsk/wordhaven/AddWordActivity.kt
+++ app/src/main/java/org/bmsk/wordhaven/AddWordActivity.kt
@@ -3,9 +3,9 @@ package org.bmsk.wordhaven
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AddWordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_word)
     }
 }
